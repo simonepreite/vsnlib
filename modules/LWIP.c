eefdeb83935ca3bd5d74f94b0b805086b0cd2aea
simonepreite <simone-preite_89@hotmail.it>
2017-11-27 21:15:18 +0100

    populate modules

--- modules/LWIP.c
+++ modules/LWIP.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <vsnlib.h>
+
+struct response* LWIP_addradddel(struct config* cfg){
+
+}
+
+struct response* LWIP_getaddr(struct config* cfg){
+
+}
+
+struct response* LWIP_adddellink(struct config* cfg){
+
+}
+
+struct response* LWIP_getsetlink(struct config* cfg){
+
+}
+
+struct response* LWIP_adddelroute(struct config* cfg){
+
+}
+
+struct response* LWIP_getroute(struct config* cfg){
+
+}
  
--- modules/picoTCP.c
+++ modules/picoTCP.c
@@ -1,1 +1,26 @@
- 
+#include <stdio.h>
+#include <vsnlib.h>
+
+struct response* picoTCP_addradddel(struct config* cfg){
+
+}
+
+struct response* picoTCP_getaddr(struct config* cfg){
+
+}
+
+struct response* picoTCP_adddellink(struct config* cfg){
+
+}
+
+struct response* picoTCP_getsetlink(struct config* cfg){
+
+}
+
+struct response* picoTCP_adddelroute(struct config* cfg){
+
+}
+
+struct response* picoTCP_getroute(struct config* cfg){
+
+}
